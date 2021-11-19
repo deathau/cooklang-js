a1f74e383b8112f22c01a60180e4f180312e1867
deathau <death_au@hotmail.com>
2021-11-19 19:26:51 +1100

    More code tidy, attempting rename to cooklang-js

--- cooklang.d.ts
+++ cooklang.d.ts
@@ -1,67 +1,71 @@
-  // a base class containing the raw string
-  export class base {
+// a base class containing the raw string
+export class base {
+  /** @public */
+  raw: string
 
-  }
+  /** @public */
+  constructor(s: string | string[])
+}
 
-  export class Recipe extends base {
-    /** @public */
-    metadata: Metadata[]
-    /** @public */
-    ingredients: Ingredient[]
-    /** @public */
-    cookware: Cookware[]
-    /** @public */
-    timers: Timer[]
-    /** @public */
-    steps: Step[]
-    /** @public */
-    image?: any
+export class Recipe extends base {
+  /** @public */
+  metadata: Metadata[]
+  /** @public */
+  ingredients: Ingredient[]
+  /** @public */
+  cookware: Cookware[]
+  /** @public */
+  timers: Timer[]
+  /** @public */
+  steps: Step[]
+  /** @public */
+  image?: any
 
-    /** @public */
-    calculateTotalTime():number
-  }
+  /** @public */
+  calculateTotalTime():number
+}
 
-  // a single recipe step
-  export class Step extends base {
-    /** @public */
-    line: (string | base)[]
-    /** @public */
-    image?: any
-  }
+// a single recipe step
+export class Step extends base {
+  /** @public */
+  line: (string | base)[]
+  /** @public */
+  image?: any
+}
 
-  // metadata
-  export class Metadata extends base {
-    /** @public */
-    key: string
-    /** @public */
-    value: string
-  }
+// metadata
+export class Metadata extends base {
+  /** @public */
+  key: string
+  /** @public */
+  value: string
+}
 
-  // ingredients
-  export class Ingredient extends base {
-    /** @public */
-    name: string
-    /** @public */
-    amount: string
-    /** @public */
-    unit: string
-  }
+// ingredients
+export class Ingredient extends base {
+  /** @public */
+  name: string
+  /** @public */
+  amount: string
+  /** @public */
+  unit: string
+}
 
-  // cookware
-  export class Cookware extends base {
-    /** @public */
-    name: string
-  }
+// cookware
+export class Cookware extends base {
+  /** @public */
+  name: string
+}
 
-  // timer
-  export class Timer extends base {
-    /** @public */
-    amount: string
-    /** @public */
-    unit: string
-    /** @public */
-    seconds: number
+// timer
+export class Timer extends base {
+  /** @public */
+  amount: string
+  /** @public */
+  unit: string
+  /** @public */
+  seconds: number
 
-    /** @public */
-    static parseTime(s: string, unit: string): number
-  }
+  /** @public */
+  static parseTime(s: string, unit: string): number
+}
