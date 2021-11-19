cbbc8f0ae7304d83a11dbcd3dd8bfd9600dcb651
deathau <death_au@hotmail.com>
2021-11-19 19:09:01 +1100

    Cody tidy, no namespace, capital classes

---
+++ cooklang.d.ts
@@ -0,0 +1,67 @@
+  // a base class containing the raw string
+  export class base {
+
+  }
+
+  export class Recipe extends base {
+    /** @public */
+    metadata: Metadata[]
+    /** @public */
+    ingredients: Ingredient[]
+    /** @public */
+    cookware: Cookware[]
+    /** @public */
+    timers: Timer[]
+    /** @public */
+    steps: Step[]
+    /** @public */
+    image?: any
+
+    /** @public */
+    calculateTotalTime():number
+  }
+
+  // a single recipe step
+  export class Step extends base {
+    /** @public */
+    line: (string | base)[]
+    /** @public */
+    image?: any
+  }
+
+  // metadata
+  export class Metadata extends base {
+    /** @public */
+    key: string
+    /** @public */
+    value: string
+  }
+
+  // ingredients
+  export class Ingredient extends base {
+    /** @public */
+    name: string
+    /** @public */
+    amount: string
+    /** @public */
+    unit: string
+  }
+
+  // cookware
+  export class Cookware extends base {
+    /** @public */
+    name: string
+  }
+
+  // timer
+  export class Timer extends base {
+    /** @public */
+    amount: string
+    /** @public */
+    unit: string
+    /** @public */
+    seconds: number
+
+    /** @public */
+    static parseTime(s: string, unit: string): number
+  }
